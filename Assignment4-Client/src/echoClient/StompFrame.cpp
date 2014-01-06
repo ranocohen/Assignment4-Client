8e7cae67070f379f7ddf2609246f0c17067b617a
idanakav <idanakav@idanakav-N55SL>
2014-01-06 18:14:05 +0200

    stompFrame abstract class added

---
+++ Assignment4-Client/src/echoClient/StompFrame.cpp
@@ -0,0 +1,18 @@
+/*
+ * StompFrame.cpp
+ *
+ *  Created on: Jan 6, 2014
+ *      Author: idanakav
+ */
+
+#include "StompFrame.h"
+
+StompFrame::StompFrame() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StompFrame::~StompFrame() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Assignment4-Client/src/echoClient/StompFrame.h
@@ -0,0 +1,17 @@
+/*
+ * StompFrame.h
+ *
+ *  Created on: Jan 6, 2014
+ *      Author: idanakav
+ */
+
+#ifndef STOMPFRAME_H_
+#define STOMPFRAME_H_
+
+class StompFrame {
+public:
+	StompFrame() =0;
+	virtual ~StompFrame();
+};
+
+#endif /* STOMPFRAME_H_ */
